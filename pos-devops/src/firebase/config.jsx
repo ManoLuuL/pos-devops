a678c318a17920dc7dabb9a3dd10ece1f98ad996
Luis Couto <ticocouto_14@hotmail.com>
2025-01-15 10:35:46 -0300

    fix: ajusta ambiente

--- pos-devops/src/firebase/config.jsx
+++ pos-devops/src/firebase/config.jsx
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     apiKey: "AIzaSyAqGbvLLoyPytkU3ryaVMigmjY-Z0TF8yU",
     authDomain: "unimar-start-devops.firebaseapp.com",
@@ -16,6 +13,9 @@ const firebaseConfig = {
     measurementId: "G-Q8VX087ZC9"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app)
+const db = getFirestore(app)
+
+export { db }
